f24303ace91381aaa8561e00e887130a03e806f9
Gulleren <gmetzsch@gmail.com>
2021-12-25 19:42:13 +0100

    Salience and engagement need to load data not scrape. readnclean json changes not working. Attempt at creating a csv from one topic w/o political leaning - csv for EC and SIS detection

--- read and clean json.R
+++ read and clean json.R
@@ -2,7 +2,7 @@ library(pacman)
 p_load(tidyverse, jsonlite)
 
 
-path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/atheism_01-01-2021_08-01-2021/atheism_01-01-2021_08-01-2021"
+path <- "/Users/gmetz/OneDrive/Dokumenter/Python Scripts/EC_Reddit_CaseStudy-main/data/Categories_raw_data/provsantigun_01-01-2019_07-01-2019/provsantigun_01-01-2019_01-07-2019"
 files <- dir(path, pattern = "*.json")
 
 data <- files %>%
@@ -13,23 +13,23 @@ data <- data$posts
 data <- data %>%
 	select(author, id, clean_text)
 
-data$pol_leaning <- 0
+#data$pol_leaning <- 0
 
-
-path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/proreligion_01-01-2021_08-01-2021/proreligion_01-01-2021_08-01-2021"
-files <- dir(path, pattern = "*.json")
-
-data_religion <- files %>%
-	map_df(~fromJSON(file.path(path, .), flatten = TRUE))
-
-data_religion <- data_religion$posts
-
-data_religion <- data_religion %>%
-	select(author, id, clean_text)
-
-data_religion$pol_leaning <- 1
-
-data_religion_vs_atheism <- full_join(data, data_religion)
+data_religion_vs_atheism <- data
+# path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/proreligion_01-01-2021_08-01-2021/proreligion_01-01-2021_08-01-2021"
+# files <- dir(path, pattern = "*.json")
+#
+# data_religion <- files %>%
+# 	map_df(~fromJSON(file.path(path, .), flatten = TRUE))
+#
+# data_religion <- data_religion$posts
+#
+# data_religion <- data_religion %>%
+# 	select(author, id, clean_text)
+#
+# data_religion$pol_leaning <- 1
+#
+# data_religion_vs_atheism <- full_join(data, data_religion)
 
 
 data_religion_vs_atheism <- data_religion_vs_atheism %>%
@@ -37,4 +37,4 @@ data_religion_vs_atheism <- data_religion_vs_atheism %>%
 	filter(lessthansix == FALSE) %>%
 	select(clean_text, pol_leaning, id)
 
-write_csv(data_religion_vs_atheism, "cleaned_atheism_religion_data_1_week")
+write_csv(data_religion_vs_atheism, "provsantigun1week.csv")
