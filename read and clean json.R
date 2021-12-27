7943a2a15f9a41f9035389e6ad7e10d0e4fcd20e
Jakobjm <43064691+Jakobjm@users.noreply.github.com>
2021-12-27 13:42:50 +0100

    fixed merging errors in read and clean json, updated NB classifier to also assign a neutral score

--- read and clean json.R
+++ read and clean json.R
@@ -2,16 +2,9 @@ library(pacman)
 p_load(tidyverse, jsonlite)
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/guncontrol_01-06-2020_01-01-2021/guncontrol_01-06-2020_01-01-2021"
 path2 <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/guncontrol_01-01-2016_01-01-2021/guncontrol_01-01-2016_01-01-2021"
-=======
-path <- "/Users/gmetz/OneDrive/Dokumenter/Python Scripts/EC_Reddit_CaseStudy-main/data/Categories_raw_data/provsantigun_01-01-2019_07-01-2019/provsantigun_01-01-2019_01-07-2019"
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
-=======
-path <- "/Users/gmetz/OneDrive/Dokumenter/Python Scripts/EC_Reddit_CaseStudy-main/data/Categories_raw_data/provsantigun_01-01-2019_07-01-2019/provsantigun_01-01-2019_01-07-2019"
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
+
 files <- dir(path, pattern = "*.json")
 files2 <- dir(path2, pattern = "*.json")
 
@@ -45,7 +38,6 @@ data_comb <- data_comb %>%
 	select(-time_period) %>%
 	distinct()
 
-<<<<<<< HEAD
 
 data_comb <- data_comb %>%
 	select(author, id, clean_text, subreddit, pol_leaning)
@@ -54,29 +46,24 @@ data_comb <- data_comb %>%
 
 
 
-<<<<<<< HEAD
-
-
-path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/proreligion_01-01-2021_08-01-2021/proreligion_01-01-2021_08-01-2021"
-files <- dir(path, pattern = "*.json")
-
-data_religion <- files %>%
-	map_df(~fromJSON(file.path(path, .), flatten = TRUE))
-
-data_religion <- data_religion$comments
-
-data_religion <- data_religion %>%
-	select(author, id, clean_text)
-
-data_religion$pol_leaning <- 1
-
-data_religion_vs_atheism <- full_join(data, data_religion)
-=======
-=======
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
-#data$pol_leaning <- 0
-
-data_religion_vs_atheism <- data
+# path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/proreligion_01-01-2021_08-01-2021/proreligion_01-01-2021_08-01-2021"
+# files <- dir(path, pattern = "*.json")
+#
+# data_religion <- files %>%
+# 	map_df(~fromJSON(file.path(path, .), flatten = TRUE))
+#
+# data_religion <- data_religion$comments
+#
+# data_religion <- data_religion %>%
+# 	select(author, id, clean_text)
+#
+# data_religion$pol_leaning <- 1
+#
+# data_religion_vs_atheism <- full_join(data, data_religion)
+#
+# #data$pol_leaning <- 0
+#
+# data_religion_vs_atheism <- data
 # path <- "../EC_Reddit_CaseStudy-main/data/Categories_raw_data/proreligion_01-01-2021_08-01-2021/proreligion_01-01-2021_08-01-2021"
 # files <- dir(path, pattern = "*.json")
 #
@@ -91,27 +78,20 @@ data_religion_vs_atheism <- data
 # data_religion$pol_leaning <- 1
 #
 # data_religion_vs_atheism <- full_join(data, data_religion)
-<<<<<<< HEAD
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
+
 
 
 data_proantiguns <- data_comb %>%
 	mutate(lessthansix = str_count(data_comb$clean_text,"\\w+")<6) %>%
-=======
-
-
-data_religion_vs_atheism <- data_religion_vs_atheism %>%
-	mutate(lessthansix = str_count(data_religion_vs_atheism$clean_text,"\\w+")<6) %>%
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
 	filter(lessthansix == FALSE) %>%
 	select(author, clean_text, pol_leaning, id)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+# data_religion_vs_atheism <- data_religion_vs_atheism %>%
+# 	mutate(lessthansix = str_count(data_religion_vs_atheism$clean_text,"\\w+")<6) %>%
+# 	filter(lessthansix == FALSE) %>%
+# 	select(author, clean_text, pol_leaning, id)
+
 write_csv(data_proantiguns, "clean_proantiguns_6months")
-=======
-write_csv(data_religion_vs_atheism, "provsantigun1week.csv")
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
-=======
-write_csv(data_religion_vs_atheism, "provsantigun1week.csv")
->>>>>>> 48f557245d5523a98e7e599f932bdeae3dafe34e
+#write_csv(data_religion_vs_atheism, "provsantigun1week.csv")
+#write_csv(data_religion_vs_atheism, "provsantigun1week.csv")
